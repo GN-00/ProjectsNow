e272c0a207d08297ca39993df26f56e0602d5e75
GN-00 <eng.hassan.alastal@gmail.com>
2021-06-01 22:13:26 +0300

    Add To Do

---
+++ ProjectsNow/ToDo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectsNow
+{
+    public class ToDo
+    {
+        //Windows.ReferencesWindows.ReferenceWindow
+        //prevent user from adding doublicat PartNumber {category + code }
+    }
+}
